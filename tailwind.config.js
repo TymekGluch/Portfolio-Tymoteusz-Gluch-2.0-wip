704fbc2d3ce1bcef61c6c6c94a9e17a175db88e1
TymekGluch <78880779+TymekGluch@users.noreply.github.com>
2024-02-22 20:35:24 +0100

    feat: add colors, darktheme colors and logic to theme menagment

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,39 @@
 /** @type {import('tailwindcss').Config} */
+
+const generateColorClass = (variable) => {
+  return ({ opacityValue }) =>
+    opacityValue
+      ? `rgba(var(--${variable}), ${opacityValue})`
+      : `rgb(var(--${variable}))`
+}
+
+const accentColor = {
+  primary: generateColorClass('accent-orange'),
+  secondary: generateColorClass('accent-green'),
+  tertiary: generateColorClass('accent-blue'),
+};
+
+const backgroundColor = {
+  primary: generateColorClass('background'),
+}
+
+const textColor = {
+  primary: generateColorClass('text'),
+}
+
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        accentColor,
+        backgroundColor,
+        textColor,
+      }
+    },
   },
   plugins: [],
 }
