c4f5c902d5634a4fe7b9725d7e8de926282d47fa
TymekGluch <78880779+TymekGluch@users.noreply.github.com>
2024-02-26 23:22:58 +0100

    feat(MainNavigation): Create majority structure and logic of MainNaviagion component

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -4,13 +4,13 @@ import { SideNavigation } from "../SideNavigation";
 
 const Layout: React.FC<React.PropsWithChildren> = ({ children }) => {
   return (
-    <article className="flex flex-col lg:justify-between align-middle gap-4 w-full max-w-screen-2xl min-h-screen border-solid border-px border-textColor-primary">
+    <article className="flex flex-col justify-end lg:justify-between align-middle gap-4 w-screen max-w-screen-2xl min-h-screen border-solid border-px border-textColor-primary">
       <MainNavigation />
 
       <div className="lg:grid grid-cols-12 gap-4">
         <SideNavigation />
 
-        <div className="col-span-8 flex flex-col gap-5 w-full min-h-[calc(100vh_-_5rem)] lg:min-h-[calc(100vh_-_5.5rem)] p-6 lg: lg:mb-2 lg:rounded-2xl border border-solid border-textColor-primary bg-transparent shadow-md-inner">
+        <div className="col-span-8 flex flex-col items-center gap-5 w-full min-h-[calc(100vh_-_5rem)] lg:min-h-[calc(100vh_-_5.5rem)] p-6 mt-20 lg:mt-0 lg:mb-2 lg:rounded-2xl border border-solid border-textColor-primary bg-transparent shadow-md-inner">
           <h1 className="text-accentColor-tertiary uppercase">
             Witaj, użytkowniku
           </h1>
@@ -28,6 +28,10 @@ const Layout: React.FC<React.PropsWithChildren> = ({ children }) => {
           </h1>
 
           <span className="block w-full h-px bg-accentColor-secondary shadow-variant-3-md" />
+
+          <div className="w-[80%] h-[100dvh] p-5 border-solid border border-accentColor-primary text-accentColor-primary shadow-variant-2-md-inner rounded-lg">
+            test section content
+          </div>
 
           {children}
         </div>
